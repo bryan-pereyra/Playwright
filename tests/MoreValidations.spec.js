3cc9fea5a19cdabe86035ea8e721716b48c125a3
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-05-05 11:18:12 -0600

    Alert popup and hover

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -7,4 +7,11 @@ test.only("PopUp Validations", async ({ page }) => {
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
+
+    // Java/JavaScript Alert PopUp
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+
+    //Mouse Hover
+    await page.locator("#mousehover").hover();
 })

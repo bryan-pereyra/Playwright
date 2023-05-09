4a6875ff9bb4cb0418fea4c08069840cca76faa4
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-05-09 10:37:04 -0600

    Parsing API response & passing token

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,13 +1,14 @@
 const { test, expect, request } = require('@playwright/test');
 const loginPayLoad = { userEmail: "play.wright@mailinator.com", userPassword: "Mypass@123" };
+let token;
 
 test.beforeAll(async () => {
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
         { data: loginPayLoad })
     expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = loginResponse.json();
-    const token = loginResponseJson.token;
+    const loginResponseJson = await loginResponse.json();
+    token = loginResponseJson.token;
     console.log(token);
 });
 
@@ -15,3 +16,71 @@ test.beforeEach(() => {
 
 });
 
+test('Place Oredr', async ({ page }) => {
+    // Passing token
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);
+
+    await page.goto("https://rahulshettyacademy.com/client/");
+
+    const email = "play.wright@mailinator.com";
+    const products = page.locator(".card-body");
+    const productName = 'adidas original';
+
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+
+    /* Dynamically find a product (For loop) */
+    const count = await products.count();
+    for (let i = 0; i < count; i++) {
+        if (await products.nth(i).locator("b").textContent() === productName) {
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
+
+    /* Add Assertions */
+    await page.locator("[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('adidas original')").isVisible();
+    expect(bool).toBeTruthy();
+
+    /* Handling Auto Suggestive dropdown options */
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").type("Me", { delay: 100 });
+    const dropdown = page.locator(".ta-results")
+    await dropdown.waitFor();
+    const countries = await dropdown.locator("button").count();
+
+    for (let i = 0; i < countries; i++) {
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if (text === " Mexico") {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    /* Store values and add assertions*/
+    await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+
+    /* Dynamically find an element (Table) */
+    await page.locator("[routerlink*='myorders']").first().click();
+    await page.locator("tbody").waitFor();
+
+    const rows = page.locator("tbody tr");
+    for (let i = 0; i < await rows.count(); i++) {
+        const orderIdRow = await rows.nth(i).locator("th").textContent();
+        if (orderId.includes(orderIdRow)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    await page.pause();
+});

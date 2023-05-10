54c1f93f9540385fccce0a121cc441c7aae3cd1c
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-05-10 17:27:21 -0600

    Screenshot & Visual comparision

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -20,4 +20,21 @@ test("PopUp Validations", async ({ page }) => {
     await framesPage.locator("li a[href*='lifetime-access']:visible").click();
     const textCheck = await framesPage.locator(".text h2").textContent();
     console.log(textCheck.split(" ")[1]);
-})
+});
+
+test("Screenshot", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#displayed-text").screenshot({ path: 'partialScreenshot.png' });
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({ path: 'screenshot.png' });
+    await expect(page.locator("#displayed-text")).toBeHidden();
+});
+
+test("Visual Comparision", async ({ page }) => {
+    await page.goto("https://www.google.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png', {
+        maxDiffPixelRatio: 0.25,
+    });
+});

adbb0386bf2fa74927108eb615d5055d36b50738
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-05-15 12:44:05 -0600

    Parameterization with different data sets

--- tests/ClientAppPracticePO.spec.js
+++ tests/ClientAppPracticePO.spec.js
@@ -3,29 +3,31 @@ const { POManager } = require('../pageobjects/POManager');
 // JSON -> String -> JS Object
 const testData = JSON.parse(JSON.stringify(require("../utils/clientAppPOTestData.json")));
 
-test('Page Object Client App Practice', async ({ page }) => {
+for (const data of testData) {
+    test(`Page Object Client App Practice: ${data.productName}` , async ({ page }) => {
 
-    // PO Implementation
-    const poManager = new POManager(page);
-    const loginPage = poManager.getLoginPage();
-    const dashboardPage = poManager.getDashboardPage();
-    const cartPage = poManager.getCartPage();
-    const ordersReviewPage = poManager.getOrdersReviewPage();
-    const ordersHistoryPage = poManager.getOrdersHistoryPage();
+        // PO Implementation
+        const poManager = new POManager(page);
+        const loginPage = poManager.getLoginPage();
+        const dashboardPage = poManager.getDashboardPage();
+        const cartPage = poManager.getCartPage();
+        const ordersReviewPage = poManager.getOrdersReviewPage();
+        const ordersHistoryPage = poManager.getOrdersHistoryPage();
 
-    await loginPage.goTo();
-    await loginPage.validLogin(testData.userName, testData.password);
+        await loginPage.goTo();
+        await loginPage.validLogin(data.userName, data.password);
 
-    await dashboardPage.searchProductAndAddToCart(testData.productName);
-    await dashboardPage.navigateToCart();
+        await dashboardPage.searchProductAndAddToCart(data.productName);
+        await dashboardPage.navigateToCart();
 
-    await cartPage.VerifyProductIsDisplayed(testData.productName);
-    await cartPage.Checkout();
+        await cartPage.VerifyProductIsDisplayed(data.productName);
+        await cartPage.Checkout();
 
-    await ordersReviewPage.searchCountryAndSelect("mex", "Mexico");
-    const orderId = await ordersReviewPage.SubmitAndGetOrderId();
-    console.log(orderId);
-    await dashboardPage.navigateToOrders();
-    await ordersHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-});
+        await ordersReviewPage.searchCountryAndSelect("mex", "Mexico");
+        const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+        console.log(orderId);
+        await dashboardPage.navigateToOrders();
+        await ordersHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+    });
+};
